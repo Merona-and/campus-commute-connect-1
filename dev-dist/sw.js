ed1e5455f6a59cdb5979349399de784fa36d6d87
merona <27merona9c@gmail.com>
2026-02-22 14:12:30 +0530

    first commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6qjnn23uhm4"
+    "revision": "0.om2h6ehqelg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
